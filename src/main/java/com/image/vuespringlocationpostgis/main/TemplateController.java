86f8473c97b96d3508876309cf9fe6f48025db62
박상준 <qkrtkdwns3410@gmail.com>
2024-10-31 20:42:12 +0900

    첫 커밋

--- src/main/java/com/image/vuespringlocationpostgis/main/TemplateController.java
+++ src/main/java/com/image/vuespringlocationpostgis/main/TemplateController.java
@@ -1,5 +1,13 @@
-package com.image.vuespringlocationpostgis.main;/**
- *packageName    : com.image.vuespringlocationpostgis.main
+package com.image.vuespringlocationpostgis.main;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * packageName    : com.image.vuespringlocationpostgis.main
  * fileName       : TemplateController
  * author         : Jun
  * date           : 24. 10. 31.
@@ -8,7 +16,17 @@ package com.image.vuespringlocationpostgis.main;/**
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 24. 10. 31.        Jun       최초 생성
- */public class TemplateController {
-
-      
+ */
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class TemplateController {
+    
+    @GetMapping("/example")
+    public String index(Model model) {
+        model.addAttribute("name", "Hello, World!");
+        model.addAttribute("title", "Vue.js + Spring Boot + PostGIS");
+        
+        return "/pages/example";
+    }
 }
