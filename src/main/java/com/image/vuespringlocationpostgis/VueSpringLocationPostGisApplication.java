3ac104dcd69530e57836801a65a4f90fc8480f64
박상준 <qkrtkdwns3410@gmail.com>
2024-10-31 20:41:50 +0900

    Initial commit

---
+++ src/main/java/com/image/vuespringlocationpostgis/VueSpringLocationPostGisApplication.java
@@ -0,0 +1,13 @@
+package com.image.vuespringlocationpostgis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringLocationPostGisApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(VueSpringLocationPostGisApplication.class, args);
+    }
+    
+}
---
+++ src/main/java/com/image/vuespringlocationpostgis/main/TemplateController.java
@@ -0,0 +1,14 @@
+package com.image.vuespringlocationpostgis.main;/**
+ *packageName    : com.image.vuespringlocationpostgis.main
+ * fileName       : TemplateController
+ * author         : Jun
+ * date           : 24. 10. 31.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 10. 31.        Jun       최초 생성
+ */public class TemplateController {
+
+      
+}
